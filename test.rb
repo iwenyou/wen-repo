238d3e9bf6a5de6b73303f018b17cc06f63e9a56
Wen Zhang <iwenyou@gmail.com>
2017-01-09 21:03:19 -0800

    add github branching

--- test.rb
+++ test.rb
@@ -7,3 +7,4 @@ Everybody is going to get food, I just wondering should I go too?
 Chaomian and fir-rice is not real chinese food, but I would eat it if I am hungry~
 when can we go home?
 I am going to keep doing it until it become instint. I mean using github.
+we are going to talk about branching in github, yay!
