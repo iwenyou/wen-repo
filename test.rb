bc9cbd77f7b00174585514b89f158b4434f8a7fe
Wen Zhang <iwenyou@gmail.com>
2017-01-09 20:44:18 -0800

    add chinese food

--- test.rb
+++ test.rb
@@ -4,3 +4,4 @@ are you there?
 anybody?
 I am just typing here to see anyone noticed.
 Everybody is going to get food, I just wondering should I go too?
+Chaomian and fir-rice is not real chinese food, but I would eat it if I am hungry~
