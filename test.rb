9418fc366268eae7cbca8cfd8cd8d53fe6968720
Wen Zhang <iwenyou@gmail.com>
2017-01-09 20:49:57 -0800

    add instint

--- test.rb
+++ test.rb
@@ -6,3 +6,4 @@ I am just typing here to see anyone noticed.
 Everybody is going to get food, I just wondering should I go too?
 Chaomian and fir-rice is not real chinese food, but I would eat it if I am hungry~
 when can we go home?
+I am going to keep doing it until it become instint. I mean using github.
