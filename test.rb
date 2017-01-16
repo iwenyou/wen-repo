9d9fa47d098a8b59d4633825874b95ba40c4a673
Wen Zhang <iwenyou@gmail.com>
2017-01-16 12:11:52 -0800

    update one week

--- test.rb
+++ test.rb
@@ -8,6 +8,37 @@ Chaomian and fir-rice is not real chinese food, but I would eat it if I am hungr
 when can we go home?
 I am going to keep doing it until it become instint. I mean using github.
 we are going to talk about branching in github, yay!
-OK, if nobody noticed then I will make this as a secert dairy for this cohort... 
+OK, if nobody noticed then I will make this as a secert dairy for this cohort...
 Some bonus interview prep tips~ (how do you see yourself in five years?)lol
   cool~
+
+  01/08/2016 Sunday
+-general introduction
+-git and github set up
+-sublime set up
+-command line set up
+
+01/09/2016 Monday
+-add SSH key to github
+-learn how to push and pull , clone and forking from github
+-some branching knowledge
+-some interview prep question
+
+01/10/2016 Tuesday
+-review today
+-demo and exercise
+-difference between symbol and string(symbol is fast)
+-getter and setter (hard way and easy way)
+-attr_reader and writer and accessor
+
+01/11/2016
+-inheritance
+-Modules
+-Splitting Files
+
+01/15/2017
+-CSS & bootstrap
+-job search
+The problem is the company problem, and your job is help with the problem.
+Convince the company you will be solve their problem.
+your best skill is attitude to learn
